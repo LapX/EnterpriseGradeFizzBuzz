8b022cc36af5dbfca69dab60eb4b5d29e27d4833
LapX <qcpredator@gmail.com>
2020-10-17 18:37:07 -0400

    Updated wrapper and setup json route

--- src/main/kotlin/jaxrs/resources/HelloWorldResource.kt
+++ src/main/kotlin/jaxrs/resources/HelloWorldResource.kt
@@ -2,9 +2,18 @@ package jaxrs.resources
 
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+data class HelloJson(val prop1: Int, val prop2: String)
 
 @Path("helloWorld")
 class HelloWorldResource {
     @GET
     fun helloWorld() = "Hello World"
+    
+    @GET
+    @Path("json")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun helloJson() = HelloJson(1, "test")
 }
